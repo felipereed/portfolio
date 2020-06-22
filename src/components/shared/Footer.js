f3179986fb1cf465825ce131e7c85dc3109980ec
Felipe Lins <contactfelipelins@gmail.com>
2020-06-21 23:12:46 -0400

    added footer links

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -5,29 +5,41 @@ export default function Footer() {
   return (
     <div>
       <div className="footer-links-container">
-        <img
-          className="footer-images"
-          src="/images/icons8-github-96.png"
-          alt="github icon"
-        ></img>
-        <img
-          className="footer-images"
-          src="/images/icons8-linkedin-circled-96.png"
-          alt="linkedin icon"
-        ></img>
-        <img
-          className="footer-images"
-          src="/images/icons8-medium-96.png"
-          alt="medium icon"
-        ></img>
-        <img
-          className="footer-images"
-          src="/images/icons8-send-email-96.png"
-          alt="send email icon"
-        ></img>
+        <a href="https://github.com/felipereed" target="_blank">
+          <img
+            className="footer-images"
+            src="/images/icons8-github-96.png"
+            alt="github icon"
+          ></img>
+        </a>
+        <a href="https://www.linkedin.com/in/felipereed" target="_blank">
+          <img
+            className="footer-images"
+            src="/images/icons8-linkedin-circled-96.png"
+            alt="linkedin icon"
+          ></img>
+        </a>
+        <a href="https://medium.com/@contactfelipelins" target="_blank">
+          <img
+            className="footer-images"
+            src="/images/icons8-medium-96.png"
+            alt="medium icon"
+          ></img>
+        </a>
+        <a href="mailto:contactfelipelins@gmail.com">
+          <img
+            className="footer-images"
+            src="/images/icons8-send-email-96.png"
+            alt="send email icon"
+          ></img>
+        </a>
       </div>
       <p>&copy;Felipe Lins. 2020.</p>
-      <p>Created with ReactJS.<br />Deployed and hosted with AWS.</p>
+      <p>
+        Created with ReactJS.
+        <br />
+        Deployed and hosted with AWS.
+      </p>
       <p>Thanks for visiting!</p>
     </div>
   );
