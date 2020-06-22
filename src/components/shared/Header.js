d9554ccdefa55da6b26d64e4520e28acb4104758
Felipe Lins <contactfelipelins@gmail.com>
2020-06-22 01:58:26 -0400

    added hamburger menu

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import "./Header.css"
+import { Link } from "react-router-dom";
+import "./Header.css";
+
 export default function Header() {
+  const toggleHamburger = (e) => {
+    document.querySelector(".hamburger").classList.toggle("is-active");
+  };
   return (
     <div>
-      <Link className="header-link" to="/home">
-        <h1>Felipe Lins</h1>
-        <h5>Software Engineer | Full stack web developer</h5>
-      </Link>
+      <div className="header-container">
+        <Link className="header-link" to="/home">
+          <h1>Felipe Lins</h1>
+        </Link>
+        <button
+          className="hamburger hamburger--spring"
+          type="button"
+          onClick={toggleHamburger}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+      </div>
+      <h5 className="header-occupation">Software Engineer | Full stack web developer</h5> 
     </div>
   );
 }
+
+
+ 
+
