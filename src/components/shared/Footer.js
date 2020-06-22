7113bcdc1054bf321f82bb7ab2b6f4eb574bdee7
Felipe Lins <contactfelipelins@gmail.com>
2020-06-21 22:55:47 -0400

    fixed footer icons

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>
-      <img src='/images/icons8-github-96.png' alt='github icon'></img>
-      <img src='/images/icons8-linkedin-circled-96.png' alt='linkedin icon'></img>
-      <img src='/images/icons8-medium-96.png' alt='medium icon'></img>
-      <img src='/images/icons8-send-email-96.png' alt='send email icon'></img>
+    <div className="footer-links-container">
+      <img className="footer-images" src='/images/icons8-github-96.png' alt='github icon'></img>
+      <img className="footer-images" src='/images/icons8-linkedin-circled-96.png' alt='linkedin icon'></img>
+      <img className="footer-images" src='/images/icons8-medium-96.png' alt='medium icon'></img>
+      <img className="footer-images" src='/images/icons8-send-email-96.png' alt='send email icon'></img>
     </div>
   )
 }
